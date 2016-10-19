2ac1e5bdfe97669ec9117babfdf3b6dd235c1805
hatuhov <hatuhov@yandex.ru>
2016-10-19 18:07:07 +0300

    w

---
+++ myup.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.min.css">
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body class="ah-myup-body">
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/wow/wow.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
+	<script>
+    	new WOW().init();
+    </script>
+
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+
+	<div class="clearfix"></div>
+
+<section class="ah-myup">
+<div class="ah-close"></div>
+	<div class="ah-myup-wrapper">
+		<span class="ah-myup-span1">Закажите звонок</span>
+		<span class="ah-myup-span2">На получение расчета стоимости и консультацию</span>
+		<form action="" class="ah-common-form">
+			<input type="text" name="uname" placeholder="Ваше имя" class="ah-uname">
+			<input type="text" name="tel" placeholder="Телефон" class="ah-tel">
+			<input type="submit" value="Заказать звонок" class="ah-submit">
+		</form>
+	</div>
+</section>
+
+
+
+	
+</body>
+</html>
+
