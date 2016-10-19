c47e35a648e0427638f92865d56d78c2aa9d3095
Berzeg07 <berezg86@gmail.com>
2016-10-19 18:00:19 +0400

    17.10

--- includes/zvonok.php
+++ includes/zvonok.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="ab-zvonok-title wow slideinDown" data-wow-duration="3.5s">
+				<div class="ab-zvonok-title wow bounceInDown" data-animated-duration="20.5s">
 					<span>Позвоните для оценки стоимости вызова<br> или оставьте заявку</span>
 				</div>
 			</div>
